2b24487b8d0a343d490774015a52d1df4b6e1694
Detective Squirrel <ratman7539@windowslive.com>
2018-01-20 21:09:22 +1300

    Add project files.

---
+++ Player Stats Dump/Core/Settings.cs
@@ -0,0 +1,22 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_Stats_Dump
+{
+    public class Settings : SettingsBase
+    {
+        public Settings()
+        {
+            TemplateSetting = false;
+        }
+
+        [Menu("First Menu", 1)]
+        public ToggleNode TemplateSetting { get; set; }
+
+    }
+}
